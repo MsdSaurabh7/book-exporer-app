f223fd23ea655be52af74bd599cdce91e527c07a
Your Name <you@example.com>
2025-06-18 09:26:12 +0530

    add capacitor for build apk for android

---
+++ book-app/android/app/src/main/java/com/msd/bookExplorer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.msd.bookExplorer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- book-app/capacitor.config.ts
+++ book-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'book-app',
+  appId: 'com.msd.bookExplorer',
+  appName: 'Book Exploring App',
   webDir: 'www'
 };
 
